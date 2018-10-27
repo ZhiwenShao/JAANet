3b70b23cb7c34162cf3d04d676d3016da26a8981
Zhiwen Shao <shaozhiwen@sjtu.edu.cn>
2018-10-27 19:05:58 +0800

    Add files via upload

---
+++ src/include/division_layer.hpp
@@ -0,0 +1,52 @@
+#ifndef CAFFE_DIVISION_LAYER_HPP_
+#define CAFFE_DIVISION_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Takes a Blob and spatially divide it into multiple divided Blob results.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class DivisionLayer : public Layer<Dtype> {
+ public:
+  explicit DivisionLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "Division"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int count_;
+  vector <int> xcoord_;
+  vector <int> ycoord_;
+  int width_;
+  int height_;
+  int num_;
+  int channels_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_DIVISION_LAYER_HPP_
