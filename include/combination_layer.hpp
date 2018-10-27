03cadf48e6aacb6b6ef0a3c6c878fce6c17872fc
Zhiwen Shao <shaozhiwen@sjtu.edu.cn>
2018-10-27 19:12:11 +0800

    Add files via upload

---
+++ include/combination_layer.hpp
@@ -0,0 +1,46 @@
+#ifndef CAFFE_COMBINATION_LAYER_HPP_
+#define CAFFE_COMBINATION_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Takes at least two Blob%s and concatenates them along either the num
+ *        or channel dimension, outputting the result.
+ */
+template <typename Dtype>
+class CombinationLayer : public Layer<Dtype> {
+ public:
+	explicit CombinationLayer(const LayerParameter& param)
+		: Layer<Dtype>(param) {}
+	virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+
+	virtual inline const char* type() const { return "Combination"; }
+
+ protected:
+	virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+	virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+	int num_, height_, width_;
+	vector <int> xcoord_;
+	vector <int> ycoord_;
+};
+
+
+}  // namespace caffe
+
+#endif  // CAFFE_COMBINATION_LAYER_HPP_
