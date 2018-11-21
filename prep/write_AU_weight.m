2c97d887f5c4459b0883b4049b1c698aec8f9c1f
Zhiwen Shao <shaozhiwen@sjtu.edu.cn>
2018-11-21 13:25:23 +0800

    Update write_AU_weight.m

--- prep/write_AU_weight.m
+++ prep/write_AU_weight.m
@@ -1,11 +1,10 @@
-clc,clear
+clc,clear;
 
-AU_num = 12;
 part_ind = [1,2,1,3,2,3];
 
 for i=1:2:length(part_ind)
     curr_AUoccur = importdata(['BP4D_combine_',num2str(part_ind(1,i)),'_',num2str(part_ind(1,i+1)),'_AUoccur.txt']);   
-       
+    AU_num = size(curr_AUoccur,2);   
     total_num = size(curr_AUoccur,1);
     
     [occur_row, occur_col] = find (curr_AUoccur>0);
@@ -17,12 +16,12 @@ for i=1:2:length(part_ind)
     end
     
     AU_weight = 1./AUoccur_rate';
-    AU_weight = AU_weight/sum(AU_weight(:))*length(AU_weight);
-    dice_weight=AU_weight/AU_num;
+    AU_weight = AU_weight/sum(AU_weight(:))*AU_num;
     dlmwrite(['BP4D_combine_',num2str(part_ind(1,i)),'_',num2str(part_ind(1,i+1)),'_weight.txt'], AU_weight, 'delimiter',' ', 'newline', 'pc');
+    % for dice loss
+    dice_weight=AU_weight/AU_num;
     
 end
 
 test_weight = ones(size(AU_weight));
 dlmwrite('BP4D_test_weight.txt', test_weight, 'delimiter',' ', 'newline', 'pc');
-    
